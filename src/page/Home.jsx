ee253696b503711bc4ac3addbd3a81b86ae1c435
kayodeolowo <officialkayodeolowo@gmail.com>
2024-01-31 15:14:34 +0100

    initial

---
+++ src/page/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='flex justify-between'>
+          <h1> NEXT DAY DELIVERY AVAILABLE </h1>
+          <h1> DISCOVER THE LATEST IT GIRL FASHION TRENDS </h1>
+          <h1> ANONYMOUS</h1>
+        </div>
+
+
+        <div>
+          <hr className='border-[1.5px] border-black'/>
+        </div>
+
+        <div>
+            <img/>
+        </div>
+    </div>
+  )
+}
+
+export default Home
